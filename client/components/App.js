c67de055f5fed81cf2d08c78733851f3dee7a326
kimromi <kimromi4@gmail.com>
2019-10-02 17:13:07 +0900

    launch server by express

---
+++ client/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import KButton from './KButton';
+
+export default class App extends React.Component {
+  render() {
+    return <div>Hello React App!<KButton>click!</KButton></div>;
+  }
+};
